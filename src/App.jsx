dbcf40d6c5bb2b60ba0dfddce6e6ca879e6cc0b1
Yinka <asyinka@gmail.com>
2023-05-27 21:37:49 +0100

    chore: installed app packages

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+const App = () => {
+  return (
+    <div className="app">
+      <div className="heading-container">
+        <h1 className="form-heading">Hello World</h1>
+        <article>A simple form to take in an input and display</article>
+      </div>
+    </div>
+  );
+};
+
+export default App;
