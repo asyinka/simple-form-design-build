bd0485117ac7eba08dcee7acff3859e4fac0074e
Yinka <asyinka@gmail.com>
2023-05-28 22:53:54 +0100

    chore: implement the event.stopPropagation on form

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
+import { useState } from "react";
 import HeadingSection from "./component/heading";
 import InputForm from "./component/inputForm";
 
-// import "./index.css";
 const App = () => {
+  const [formDisplay, setFormDisplay] = useState(false);
   return (
     <div className="app">
       <HeadingSection />
-      <div className="panel-heading">Click to enter input content</div>
-      <InputForm />
+      <div onClick={() => setFormDisplay(true)} className="panel-heading">
+        Click to enter input content
+      </div>
+      {formDisplay && <InputForm setFormDisplay={setFormDisplay} />}
     </div>
   );
 };
--- src/component/inputForm.jsx
+++ src/component/inputForm.jsx
@@ -1,10 +1,17 @@
-const InputForm = () => {
+const InputForm = ({ setFormDisplay }) => {
   return (
-    <form className="app-form">
-      <label htmlFor="input">Type in your input</label>
-      <input id="input" className="form-input" type="text" />
-      <button className="form-button">Click to add Input</button>
-    </form>
+    <div className="app-form-bg" onClick={() => setFormDisplay(false)}>
+      <form onClick={(event) => event.stopPropagation()} className="app-form">
+        <label htmlFor="input">Type in your input</label>
+        <input
+          id="input"
+          placeholder="Enter your text here..."
+          className="form-input"
+          type="text"
+        />
+        <button className="form-button">Click to add Input</button>
+      </form>
+    </div>
   );
 };
 
